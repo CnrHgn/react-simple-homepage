9a51f3cb2ec999889747260ed112ece300d834c8
Conor Hagan <conorhagan7@gmail.com>
2021-12-15 14:37:02 +0000

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { RiMenu3Line, RiCLoseLin} from 'react-icons';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        
+        <div className='gpt3__navbar-links_logo'>
+          {/* <img src={logo} alt="Logo" /> */}
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/containers/whatCNR/WhatCNR.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './whatCNR.css';
+
+const WhatCNR = () => {
+  return (
+    <div>
+      WhatCNR
+    </div>
+  )
+}
+
+export default WhatCNR
